6bbcaab980c2ae4e807f18ee1244b556986b6165
mukmukmukmuk <ksm010825@naver.com>
2023-11-10 23:37:34 +0900

    이미지 파일 해결

--- Api/urls.py
+++ Api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 # router = DefaultRouter()
 # router.register(r'plant', PlantViewSet)
@@ -19,3 +21,4 @@ urlpatterns = [
     path('species/', SpeciesViewSet.as_view()),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
