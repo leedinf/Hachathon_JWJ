25f0b2982467ddba3005c4ed770cd3f43c9fbed3
mukmukmukmuk <ksm010825@naver.com>
2023-11-10 15:14:40 +0900

    완성!!

--- Api/models.py
+++ Api/models.py
@@ -4,14 +4,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 def user_directory_path(instance, filename):
     birthday = instance.birthday.strftime('%Y/%m')
-    return 'user_{0}/{1}'.format(instance.user.id, birthday, filename)
+    return '{0}/{1}'.format(birthday, filename)
 
 
 class Plant(models.Model):
     nickname = models.CharField(max_length=15)
-    content = models.TextField(blank=True)
     image = models.ImageField(upload_to=user_directory_path, blank=True)
-    species = models.ForeignKey("Species",  on_delete=models.CASCADE)
+    species = models.ForeignKey(
+        "Species",  on_delete=models.CASCADE, related_name='plants')
     interest = models.PositiveSmallIntegerField(default=0)
     humidity = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(100)])
@@ -26,6 +26,7 @@ class Species(models.Model):
     name = models.CharField(max_length=30)
     good_humidity = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(100)])
+    content = models.TextField()
 
     def __str__(self):
         return self.name
--- Api/views.py
+++ Api/views.py
@@ -1,20 +1,71 @@
 from django.shortcuts import render
 from .serializers import *
-from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from .models import Plant, Species, Feedback
-# Create your views here.
+from rest_framework.generics import UpdateAPIView, CreateAPIView, ListAPIView
+
+# class PlantViewSet(viewsets.ModelViewSet):
+#    queryset = Plant.objects.all()
+#    serializer_class = PlantSerializer
+#
+# class SpeciesViewSet(viewsets.ModelViewSet):
+#    queryset = Species.objects.all()
+#    serializer_class = SpeciesSerializer
+#
+# class FeedbackViewSet(viewsets.ModelViewSet):
+#    queryset = Feedback.objects.all()
+#    serializer_class = FeedbackSerializer
 
 
-class PlantViewSet(viewsets.ModelViewSet):
-    queryset = Plant.objects.all()
-    serializer_class = PlantSerializer
+class PlantViewSet(ModelViewSet):
+    queryset = Plant.objects.all().order_by('interest')
+    serializer_class = PlantListSerializer
+
+    def get_serializer_context(self):
+        return {
+            'request': None,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+    def get_queryset(self):
+        return Plant.objects.all().select_related('species').prefetch_related('feedback_set')
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        feedback = instance.feedback_set.all()
+        species = instance.species
+        dataDict = {
+            'plant': instance,
+            'feedback': feedback,
+            'species': species,
+        }
+        serializers = PlantRetrieveSerializer(dataDict).data
+        return Response(serializers)
+
+    def interest(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.interest += 1
+        instance.save()
 
 
-class SpeciesViewSet(viewsets.ModelViewSet):
+class SpeciesViewSet(ListAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
 
 
-class FeedbackViewSet(viewsets.ModelViewSet):
+class WateringViewset(UpdateAPIView):
+    queryset = Plant.objects.all()
+    serializer_class = WateringSerializer
+
+
+class FeedbackViewset(CreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+
+
+class HumidityViewset(UpdateAPIView):
+    queryset = Plant.objects.all()
+    serializer_class = WateringSerializer
